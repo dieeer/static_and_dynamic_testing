1e30f0f8c3ed5f086205d42c848e9dabb77bbc57
justin reid <hi@justinre.id>
2022-11-14 09:45:18 +0000

    init commit

---
+++ part_1_docs/test.py
@@ -0,0 +1,26 @@
+class CardGame:
+
+
+  def check_for_ace(self, card):
+    #   v should be == instead of = for conditional checking
+    if card.value = 1:
+      return True
+    else
+      return False
+
+# def is misspelt, there is no comma between card1 and card2
+  dif highest_card(self, card1 card2):
+  if card1.value > card2.value:
+    #   card is not declared as an argument, only card1 and card2 are
+    return card
+  else:
+    return card2
+
+
+
+def cards_total(self, cards):
+    # total variable is not declared properly, needs to be assigned value
+  total
+  for card in cards:
+    total += card.value
+    return "You have a total of" + total
